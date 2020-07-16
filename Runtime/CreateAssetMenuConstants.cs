e3eb183a8c858f78c36c00519c3cf49e320915ae
Vladimir Popov <zor1994@gmail.com>
2020-07-16 22:08:02 +0300

    Continuous typo fix

--- Runtime/CreateAssetMenuConstants.cs
+++ Runtime/CreateAssetMenuConstants.cs
@@ -6,7 +6,7 @@ namespace Zor.RandomGenerators
 	{
 		public const string RandomGeneratorProvidersFolder = "Random Generator Providers/";
 
-		public const string ContinuousDistributionsFolder = RandomGeneratorProvidersFolder + "Continous Distributions/";
+		public const string ContinuousDistributionsFolder = RandomGeneratorProvidersFolder + "Continuous Distributions/";
 
 		public const string NormalDistributionsFolder = ContinuousDistributionsFolder + "Normal Distributions/";
 		public const string BatesDistributionFolder = NormalDistributionsFolder + "Bates Distribution/";

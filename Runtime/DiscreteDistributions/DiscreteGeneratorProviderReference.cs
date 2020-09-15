ea5b2d4b9fa4c669d3e00964cf27e859194bfc03
Vladimir Popov <zor1994@gmail.com>
2020-09-15 09:44:06 +0300

    DiscreteGeneratorProviderReference optimization

--- Runtime/DiscreteDistributions/DiscreteGeneratorProviderReference.cs
+++ Runtime/DiscreteDistributions/DiscreteGeneratorProviderReference.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2020 Vladimir Popov zor1994@gmail.com https://github.com/ZorPastaman/Random-Generators
 
 using System;
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 using UnityEngine;
 using Zor.RandomGenerators.PropertyDrawerAttributes;
@@ -14,12 +15,19 @@ namespace Zor.RandomGenerators.DiscreteDistributions
 	/// </summary>
 	/// <seealso cref="RequireDiscreteGenerator"/>.
 	[Serializable]
-	public struct DiscreteGeneratorProviderReference
+	public struct DiscreteGeneratorProviderReference : IEquatable<DiscreteGeneratorProviderReference>
 	{
 #pragma warning disable CS0649
-		[SerializeField] private DiscreteGeneratorProvider_Base m_DiscreteGeneratorProvider;
+		[SerializeField, NotNull] private DiscreteGeneratorProvider_Base m_DiscreteGeneratorProvider;
 		[SerializeField] private bool m_Shared;
 #pragma warning restore CS0649
+
+		public DiscreteGeneratorProviderReference([NotNull] DiscreteGeneratorProvider_Base discreteGeneratorProvider,
+			bool shared)
+		{
+			m_DiscreteGeneratorProvider = discreteGeneratorProvider;
+			m_Shared = shared;
+		}
 
 		/// <summary>
 		/// Returns a shared or not <see cref="IDiscreteGenerator{T}"/> by the specified parameter.
@@ -29,7 +37,7 @@ namespace Zor.RandomGenerators.DiscreteDistributions
 		/// <remarks>
 		/// It's recommended to cache the result.
 		/// </remarks>
-		[CanBeNull]
+		[Pure]
 		public IDiscreteGenerator<T> GetGenerator<T>()
 		{
 			if (m_DiscreteGeneratorProvider is DiscreteGeneratorProvider<T> discreteGeneratorProvider)
@@ -38,6 +46,59 @@ namespace Zor.RandomGenerators.DiscreteDistributions
 			}
 
 			return null;
+		}
+
+		[NotNull]
+		public DiscreteGeneratorProvider_Base continuousGeneratorProvider
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
+			get => m_DiscreteGeneratorProvider;
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			set => m_DiscreteGeneratorProvider = value;
+		}
+
+		public bool shared
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
+			get => m_Shared;
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			set => m_Shared = value;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
+		public bool Equals(DiscreteGeneratorProviderReference other)
+		{
+			return m_DiscreteGeneratorProvider.Equals(other.m_DiscreteGeneratorProvider) & m_Shared == other.m_Shared;
+		}
+
+		[Pure]
+		public override bool Equals(object obj)
+		{
+			return obj is DiscreteGeneratorProviderReference other && Equals(other);
+		}
+
+		[Pure]
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return ((m_DiscreteGeneratorProvider != null ? m_DiscreteGeneratorProvider.GetHashCode() : 0) * 397)
+					^ m_Shared.GetHashCode();
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
+		public static bool operator ==(DiscreteGeneratorProviderReference left,
+			DiscreteGeneratorProviderReference right)
+		{
+			return left.Equals(right);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining), Pure]
+		public static bool operator !=(DiscreteGeneratorProviderReference left,
+			DiscreteGeneratorProviderReference right)
+		{
+			return !left.Equals(right);
 		}
 	}
 }
